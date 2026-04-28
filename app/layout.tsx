a52abdcdf6a003868e9b508782a25232c441bed0
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2026-04-28 12:40:42 +0530

    auth setsssss userr eror111

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
-import "./globals.css";
+ import "./globals.css";
 import Providers from "./Providers";
+import { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
